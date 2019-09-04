e85c7baf4a5a9a284edefedce67052787684fe98
jerry <309032663@qq.com>
2019-09-04 09:06:32 +0800

    update

--- app/src/main/java/com/cxyzy/demo/MainActivity.kt
+++ app/src/main/java/com/cxyzy/demo/MainActivity.kt
@@ -5,41 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val minSpeed = 1
-    private val maxSpeed = 10
-    var speed = minSpeed
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startAnimation()
-        accelerateBtn.setOnClickListener { accelerate() }
-        slowDownBtn.setOnClickListener { slowDown() }
+
+        circleProgress.value = 50f
+//        circleProgress.hint = "dddd"
     }
 
-    private fun accelerate() {
-        if (speed < maxSpeed) {
-            speed++
-        }
-        speedTv.text = speed.toString()
-        startAnimation()
-        updateButtons()
-    }
-
-    private fun slowDown() {
-        if (speed > minSpeed) {
-            speed--
-            speedTv.text = speed.toString()
-            startAnimation()
-        }
-        updateButtons()
-    }
-
-    private fun startAnimation() {
-        doubleWindmillView.startRotate(speed)
-    }
-
-    private fun updateButtons() {
-        accelerateBtn.isEnabled = speed < maxSpeed
-        slowDownBtn.isEnabled = speed > minSpeed
-    }
 }
---
+++ library/src/main/java/com/cxyzy/library/Constant.java
@@ -0,0 +1,35 @@
+package com.cxyzy.library;
+
+/**
+ * Created by wyg on 2017/7/30.
+ */
+
+public class Constant {
+
+    public static final boolean ANTI_ALIAS = true;
+
+    public static final int DEFAULT_SIZE = 150;
+    public static final int DEFAULT_START_ANGLE = 270;
+    public static final int DEFAULT_SWEEP_ANGLE = 360;
+
+    public static final int DEFAULT_ANIM_TIME = 1500;
+
+    public static final int DEFAULT_MAX_VALUE = 100;
+    public static final int DEFAULT_VALUE = 50;
+
+    public static final int DEFAULT_HINT_SIZE = 25;
+    public static final int DEFAULT_UNIT_SIZE = 30;
+    public static final int DEFAULT_VALUE_SIZE = 40;
+    public static final int DEFAULT_TEXT_SIZE = 30;
+
+    public static final int DEFAULT_ARC_WIDTH = 15;
+
+
+    public static final String WEATHER_URL = "https://free-api.heweather.com/v5/";
+    public static final String LOCATE_KEY = "dc60aaa882c6854f93ac7e3d55516f58";
+    public static final String WEATHER_KEY = "1c8bebce635648809e98babb820856c9";
+    public static final String BASE_URL = "http://oy5qvvdsx.bkt.clouddn.com/oneweather/";
+    public static final String CONDITION_ICON = BASE_URL + "condition_icon/";
+    public static final String BACKGROUD_IAMGE = BASE_URL + "image/jpg/";
+    
+}
