516a64ece43fbd10a2b996f8358909a1c664d0b3
jerry <309032663@qq.com>
2019-09-05 14:20:06 +0800

    update

--- app/src/main/java/com/cxyzy/demo/MainActivity.kt
+++ app/src/main/java/com/cxyzy/demo/MainActivity.kt
@@ -3,12 +3,20 @@ package com.cxyzy.demo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        circleProgress.startAnimation()
+        var sunriseTime = "08:00"
+        var sunsetTime = "19:00"
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.CHINA)
+        val nowDate = Date(System.currentTimeMillis())
+        val nowTime = dateFormat.format(nowDate)
+        sunriseSunset.setTime(sunriseTime, sunsetTime, nowTime)
+        sunriseSunset.startAnimation()
     }
 
 }
